5fad6b7240ec2c12904943e3984fd542b85d146a
timomer <tim@twoit.co.uk>
2016-02-02 10:30:33 +0000

    Moved to ObjectToSync object

--- app/src/main/java/com/hypodiabetic/pumpdriverexample/IncomingService.java
+++ app/src/main/java/com/hypodiabetic/pumpdriverexample/IncomingService.java
@@ -16,19 +16,12 @@ import android.widget.Toast;
 
 import com.google.gson.reflect.TypeToken;
 import com.hypodiabetic.pumpdriverexample.Objects.Basal;
-import com.hypodiabetic.pumpdriverexample.Objects.BasalSync;
-
-import org.json.JSONArray;
-import org.json.JSONException;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.hypodiabetic.pumpdriverexample.Objects.BolusSync;
 import com.hypodiabetic.pumpdriverexample.Objects.ObjectToSync;
 import com.hypodiabetic.pumpdriverexample.Objects.Treatment;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
